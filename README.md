bafbbb6fd1e504a70478d1899f3c3aa07059fcdb
Sim Boon Hwa <simboonhwa@rocketmail.com>
2015-03-21 23:10:31 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# Mod3Assigment
-Mod3 Assigment
+----------
+# **GETTING AND CLEANING DATA** #
+# Course Project #
+----------
+
+run_analysis.R that does the following:
+
+1. Merges the training and the test sets to create one data set. *[Remark 2]*
+2. Extracts only the measurements on the mean and standard deviation for each measurement. *[Remark 1]*
+3. Uses descriptive activity names to name the activities in the data set. *[Remark 3]*
+4. Appropriately labels the data set with descriptive variable names.*[Remark 4]*
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject. *[Remark 5]*
+
+
+*"remark 1,2,3,4 & 5" refer to comment in the "run_analysis" script*
+
+----------
+
+
+**Remark 1 section**
+
+----------
+
+From feature.txt, the script will extract and construct the column position and respective description. These will be used to extract respective column in dataset as well as for column naming.  
+
+
+----------
+
+**Remark 2 section**
+
+----------
+
+Dataset (X,Y and subject) from train and test are merged into single X, Y and subject data frame. 
+
+----------
+
+**Remark 3 section**
+
+----------
+
+Descriptions that are extracted during Remark 1 are used here to populate the column name as to provide meaningful description
+
+----------
+
+**Remark 4 section**
+
+----------
+
+Dataset are translated into meaningful description
+
+----------
+
+**Remark 5 section**
+
+----------
+
+Data frames (X,Y & subject) are merged to form a single data frame. Mean are computed by grouping subject and activity
+
+----------
+
+The dataset includes the following files:
+=========================================
+
+- 'README.md'
+
+- 'code book.doc': Shows information about the variables used on the feature vector.
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,48 +1,70 @@
 run_analysis <- function(){
 
-# ------------------prepare the features column-----------------------
-feature.df <- read.csv("./RAW/features.txt", stringsAsFactors=F,sep=" ",header=F)
-pattern <- "mean|std"
-feature.extract.df <- feature.df[grep(pattern,feature.df$V2),]
-# ---- extract the row number with mean & std ------------------
-extract.l <- feature.extract.df$V1
-# ---- extract description with mean & std --------------------
-feature.df$extract <- feature.df$V1 %in% extract.l
+#########################################################################
+# 	****************  Remark 1 *****************
+# 	Base on information from feature.txt, key words "mean" & "std" 
+#	are extract and filtered. The description will be used to polluate the 
+#	column and position will use to determin which column to extract
+#########################################################################
+# --- convert the boolean to 16 or -16. This will use pass as width ----
+# --- arg im read.fwf --------------------------------------------------
 library(plyr)
-# ---- convert the boolean to 16 or -16 ------------------------
 xwidth.f <- revalue(factor(feature.df$extract), c("TRUE"=16,"FALSE"=-16))
 xwidth <- as.numeric(levels(xwidth.f)[as.numeric(xwidth.f)])
-#x.train <- read.fwf("./RAW/train/X_train.txt", width=xwidth, header=F)
-x.train <- read.fwf("./RAW/train/X_train.txt", width=xwidth, header=F, skip=7000)
-x.test<- read.fwf("./RAW/test/X_test.txt", width=xwidth, header=F, skip=2000)
-#x.test<- read.fwf("./RAW/test/X_test.txt", width=xwidth, header=F)
+
+#########################################################################
+# 	****************  Remark 2 *****************
+# 	Read in the train & test dataset merge them together
+#########################################################################
+
+#--------- Read in x dataset ----------------------------------------- 
+x.train <- read.fwf("./RAW/train/X_train.txt", width=xwidth, header=F)
+x.test<- read.fwf("./RAW/test/X_test.txt", width=xwidth, header=F)
 x.all <- rbind(x.train, x.test)
-# ---- assign the column name ------------------------
-colnames(x.all) <- gsub("\\()","",feature.extract.df$V2)  
-#y.train <- read.csv("./RAW/train/y_train.txt", stringsAsFactors=F, header=F)  
-y.train <- read.csv("./RAW/train/y_train.txt", stringsAsFactors=F, header=F, skip=7000)  
-y.test  <- read.csv("./RAW/test/y_test.txt", stringsAsFactors=FALSE, header=F, skip=2000)
-#y.test  <- read.csv("./RAW/test/y_test.txt", stringsAsFactors=FALSE, header=F)
+
+#--------- Read in y dataset ----------------------------------------- 
+y.train <- read.csv("./RAW/train/y_train.txt", stringsAsFactors=F, header=F)  
+y.test  <- read.csv("./RAW/test/y_test.txt", stringsAsFactors=FALSE, header=F)
 y.all <- rbind(y.train, y.test)
 
+#--------- Read in subject dataset ----------------------------------------- 
+subject.train  <- read.csv("./RAW/train/subject_train.txt", stringsAsFactors=F, header=F)
+subject.test  <- read.csv("./RAW/test/subject_test.txt", stringsAsFactors=F, header=F)
+subject.all <- rbind(subject.train, subject.test)
+
+#########################################################################
+# 	****************  Remark 3 *****************
+# 	create descriptive column and value
+#########################################################################
+# ---- assign descriptive column name to respective DF -----------------
+colnames(x.all) <- gsub("\\()","",feature.extract.df$V2)  
+
+colnames (subject.all) <- "Subject"
+
+#########################################################################
+# 	****************  Remark 4 *****************
+# 	Labels the dataset with activity name
+#########################################################################
 activities.map <- c("1"="WALKING","2"="WALKING_UPSTAIR","3"="WALKING_DOWNSTAIRS","4"="SITTING","5"="STANDING","6"= "LAYING")
 y.f <- revalue(factor(y.all$V1),activities.map)
 y.all <- data.frame(y.f)
 colnames(y.all)<-"activity"
     
-#subject.train  <- read.csv("./RAW/train/subject_train.txt", stringsAsFactors=F, header=F)
-subject.train  <- read.csv("./RAW/train/subject_train.txt", stringsAsFactors=F, header=F, skip=7000)
-subject.test  <- read.csv("./RAW/test/subject_test.txt", stringsAsFactors=F, header=F, skip=2000)
-#subject.test  <- read.csv("./RAW/test/subject_test.txt", stringsAsFactors=F, header=F)
-subject.all <- rbind(subject.train, subject.test)
-colnames (subject.all) <- "Subject"
+
+# ---- merge all three DF into one big DF -------------------------------
 consolidate.df <- data.frame(subject.all,y.all,x.all)	
 
+#########################################################################
+# 	****************  Remark 5 *****************
+# 	mean are computed for each column group accordingly
+#       to subject and activity
+#########################################################################
 variable.l <- list(Groupsubject=consolidate.df$Subject, GroupActivity=consolidate.df$activity)
 consolidate.mean.df <- aggregate(consolidate.df, by=variable.l,FUN=mean, na.rm=TRUE)
 
+# ---- remove both columns subject & activity -------------------------------
 consolidate.mean.df$Subject <- NULL
 consolidate.mean.df$activity <- NULL
 
-#write.table(consolidate.mean.df, file = "MyData.txt",row.names=FALSE)
+write.table(consolidate.mean.df, file = "MyData.txt",row.names=FALSE)
 }
